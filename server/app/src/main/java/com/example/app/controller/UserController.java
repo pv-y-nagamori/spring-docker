de9e56eadd199f89c7630a875719b4fd65ed46a9
pv-y-nagamori <y-nagamori@pro-vision.jp>
2025-01-31 13:46:37 +0900

    tymeleaf

--- server/app/src/main/java/com/example/app/controller/UserController.java
+++ server/app/src/main/java/com/example/app/controller/UserController.java
@@ -1,22 +1,35 @@
+// package com.example.app.controller;
+
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+// import com.example.app.repository.UserRepository;
+
+// @RestController
+// public class UserController {
+
+//   private final UserRepository repository;
+
+//   @Autowired
+//   public UserController(UserRepository repository) {
+//     this.repository = repository;
+//   }
+
+//   @RequestMapping("/")
+//   public String user() {
+//     return String.valueOf(repository.findAll());
+//   }
+// }
+
 package com.example.app.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import com.example.app.repository.UserRepository;
-
-@RestController
+@Controller
 public class UserController {
-
-  private final UserRepository repository;
-
-  @Autowired
-  public UserController(UserRepository repository) {
-    this.repository = repository;
-  }
-
-  @RequestMapping("/")
-  public String user() {
-    return String.valueOf(repository.findAll());
-  }
+    @GetMapping("/")
+    public String helloView(){
+        // 戻り値は「ビュー名」を返す
+        return "hello";
+    }
 }

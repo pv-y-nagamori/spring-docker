01c2dcc48bdf55c7f90be20be270d3f9e7dd0700
pv-y-nagamori <y-nagamori@pro-vision.jp>
2025-02-03 15:35:09 +0900

    log機能実装

--- server/app/src/main/java/com/example/app/controller/LoginController.java
+++ server/app/src/main/java/com/example/app/controller/LoginController.java
@@ -6,8 +6,13 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Controller // このクラスがWebコントローラーであることを示します
 public class LoginController {
+
+    private final Logger logger = LoggerFactory.getLogger(LoginController.class); // ログ インスタンス
 
     @GetMapping("/login") // "/login"というURLに対するGETリクエストを処理します
     public String login() {
@@ -18,6 +23,7 @@ public class LoginController {
     public String redirectToIndex() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication(); // 現在のユーザーの認証情報を取得します
         if (authentication != null && authentication.isAuthenticated()) { // ユーザーがログインしている場合
+            logger.info("login!!");
             return "redirect:/index";  // "/index"にリダイレクトします
         }
         return "redirect:/login"; // ユーザーがログインしていない場合、"/login"にリダイレクトします
